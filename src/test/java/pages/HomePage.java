2287d5ac99b861a9271a17670d7d18ad33cce7fc
Zinaida <zmileshin@gmail.com>
2024-07-15 14:48:58 -0400

    Added test to verify number of categories on Homepage

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -19,6 +19,44 @@ private WebElement logo;
 public void clickOnLogo(){
     logo.click();
 }
-   // private WebElement RestaurantEquipment;
+
+@FindBy ( xpath= "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Restaurant Equipment')]")
+private WebElement RestaurantEquipment;
+
+@FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Commercial Refrigeration')]")
+private WebElement CommercialRefrigeration;
+
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Smallwares')]")
+    private WebElement Smallwares;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Storage & Transport')]")
+    private WebElement StorageAndTransport;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Dinnerware & Tabletop')]")
+    private WebElement DinnerwareAndTabletop;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Disposables')]")
+    private WebElement Disposables;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Furniture')]")
+    private WebElement Furniture;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Industrial Supplies')]")
+    private WebElement IndustrialSupplies;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Food & Beverage')]")
+    private WebElement FoodAndBeverage;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Janitorial Supplies')]")
+    private WebElement JanitorialSupplies;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Parts & Accessories')]")
+    private WebElement PartsAndAccessories;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Event Furniture')]")
+    private WebElement EventFurniture;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Coffee and Espresso')]")
+    private WebElement CoffeeAndEspresso;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Cooking Utensils')]")
+    private WebElement CookingUtensils;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Plastic Cups and Lids')]")
+    private WebElement PlasticCupsAndLids;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Retail Shelving')]")
+    private WebElement RetailShelving;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'Business Type')]")
+    private WebElement BusinessType;
+    @FindBy (xpath = "//p[@class='text-sm leading-none text-gray-900 group-hover:text-green-500 group-hover:underline mb-0 p-1 transition-all duration-75 ease-in-out mt-2 lt:mt-5' and contains(text(), 'New Items')]")
+    private WebElement NewItems;
 
 }
