5ff11f3204410810214b0c56f9bdc990c1c1f6ba
Kenan Mammadli <kenan@doe.org>
2024-07-17 21:42:27 -0400

    updated properties file, added new framework constants, updated products page and tests

--- src/test/java/utilities/FrameworkConstants.java
+++ src/test/java/utilities/FrameworkConstants.java
@@ -4,5 +4,14 @@ public class FrameworkConstants {
 
     public static final String HOMEPAGE_URL = "https://www.webstaurantstore.com/";
 
+    public static final String EQUIPMENT_FAMILY = "Restaurant Equipment";
+
+    public static final String EQUIPMENT_CARD = "Cooking Equipment";
+
+    public static final String PRODUCT_NAME = "Commercial Gas Ranges";
+
+
+
+
 
 }
